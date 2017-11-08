274de51feb896f60fc1faf2200dac8006438cbd0
Miguel Debono <a101317@mcast.org.mt>
2017-11-08 10:54:52 +0100

    Added my unity games

---
+++ BrickGame/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+   public void LoadNewScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ BrickGame/Assets/Scripts/MusicPlayerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayerScript : MonoBehaviour {
+    static MusicPlayerScript instance = null;
+
+    private void Awake()
+    {
+
+        if (instance == null)
+        {
+            instance = this; //this current object(mymusicplayer)
+            DontDestroyOnLoad(gameObject);
+        }
+        else //if not null, then a music player is already running
+        {
+            Destroy(gameObject); ;
+            print("Duplicate Music Player is self destructing!");
+
+        }
+    }
+    // Use this for initialization
+    void Start () {
+
+        }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
