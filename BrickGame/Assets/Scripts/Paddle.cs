05f5a16106da755607dec8bf86afab07bce27171
Miguel Debono <a101317@mcast.org.mt>
2017-11-10 14:24:17 +0100

    -Set paddle to move with mouse position(in game units) - Locked the ball to move with the paddle at the start of the game -Upon first left mouse button click, the ball should launch
    Signed-off-by: Miguel Debono <a101317@mcast.org.mt>

---
+++ BrickGame/Assets/Scripts/Paddle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        print(Input.mousePosition.x);
+
+        float mousePosInUnits = (Input.mousePosition.x / Screen.width * 16) - 8;
+
+       Vector3 newPaddlePos =  this.transform.position = new Vector3(mousePosInUnits, this.transform.position.y, this.transform.position.z);
+
+
+       newPaddlePos.x = Mathf.Clamp(mousePosInUnits, -7.36f, 7.36f);
+        this.transform.position = newPaddlePos;
+    }
+}
---
+++ BrickGame/Assets/Scripts/ball.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour {
+    public Paddle paddle;
+    Vector3 paddleBallPosDiff;
+    bool gamestarted = false;
+
+	// Use this for initialization
+	void Start () {
+        paddleBallPosDiff = this.transform.position - paddle.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!gamestarted)
+        {
+            this.transform.position = paddle.transform.position + paddleBallPosDiff;
+        }
+
+        if (Input.GetMouseButtonDown(0) && !gamestarted) 
+        {
+            gamestarted = true;
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 10f) ;
+        }
+
+        }
+}
